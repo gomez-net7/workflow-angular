900ea60c86475176d2c3a9d0d718b888b20aab38
Edgar Gomez <gomez@netseven.it>
2022-02-08 10:46:14 +0100

    fix build scripts

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const PKG_VERSION = process.env.npm_package_version;
-const packages = ['common', 'muruca'];
+const packages = ['common', 'muruca', 'dataviz'];
 
 const pkgUpdate$ = packages.map((pkg) => {
   const pkgPath = path.join(path.dirname(fs.realpathSync(__filename)), `../projects/${pkg}/package.json`);
